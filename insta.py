b6de749445a1fcee9f7c4dce1b0ae6c7e087974a
FaceLessOfficialNeq <151958189+FaceLessOfficialNew@users.noreply.github.com>
2023-12-12 00:09:08 +0600

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,149 @@
+import os, sys, requests, time, instagrapi
+from colorama import Fore
+from instagrapi import Client
+from datetime import datetime
+
+def save_attempt(user_input):
+    timestamp = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+    with open("attempts.txt", "a") as file:
+        file.write(f"'{user_input}' || date: {timestamp}\n")
+
+# By TnYtCoder (GitHub)
+# Instagrapi Used !!
+
+time.sleep(1)
+os.system('clear')
+time.sleep(1)
+
+class fg:
+    BLACK   = '\033[30m'
+    RED     = '\033[31m'
+    GREEN   = '\033[32m'
+    YELLOW  = '\033[33m'
+    BLUE    = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN    = '\033[36m'
+    WHITE   = '\033[37m'
+    RESET   = '\033[39m'
+
+def typewriter(message):
+    for char in message:
+        print(char, end='', flush=True)
+        time.sleep(0.01)
+    print()
+
+def banner():
+	logo = '''
+\033[92m 
+██╗███╗   ██╗███████╗████████╗ █████╗       ███████╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗      ██╔════╝
+██║██╔██╗ ██║███████╗   ██║   ███████║█████╗█████╗  
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║╚════╝██╔══╝  
+██║██║ ╚████║███████║   ██║   ██║  ██║      ██║     
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝      ╚═╝     For Increase Instagram Followers
+                                                    
+                                                    
+                                                
+   ---------------------------------------
+   [+]Website:https://facelesshackingofficial.blogspot.com
+   
+   [+]Follow Or Fb Page:https://m.facebook.com/groups/1027667035104862/?ref=share&mibextid=NSMWBT
+   
+   [+]For Motivation: You Become Dangerous When You Fuck Your Emotions 😎
+ 
+	'''
+	typewriter(logo)
+
+banner()
+
+
+with open('credentials.ini', 'r') as f:
+  contents = f.read()
+
+lines = contents.splitlines()
+
+username = lines[1]
+password = lines[2]
+
+#api
+api = Client()
+
+if __name__ == "__main__":
+    user_input = username
+    save_attempt(user_input)
+
+print("\033[91mAttempting Login !! ")
+
+try:
+	api.login(username, password)
+	print("\033[33mLogin Successful !\n")
+except:
+	print('Login Failed !?')
+	print('Please Check Your Username & Password\n')
+	sys.exit()
+
+def celeb():
+        try:
+                user_id = api.user_id_from_username("iamduzz0909")
+                followings = api.user_following(user_id)
+                followings_ids = list(followings.keys())
+
+                for u_to_follow in followings_ids:
+                        try:
+                                print(f"\033[36m[>] Trying To Follow Celebrity")
+                                api.user_follow(u_to_follow)
+                        except instagrapi.exceptions.UserNotFound:
+                                print("\033[31m[>] User Not Found !")
+                        except instagrapi.exceptions.PleaseWaitFewMinutes:
+                                print("\n\033[31m[>] Please Try Again After Few Minute !")
+                                break
+        except KeyboardInterrupt:
+                print("\n\033[31m[>] Keyboard Interrupt : Script Ended !")
+                sys.exit()
+        except instagrapi.exceptions.UserNotFound:
+                print("\n\033[31m[>] User Not Found !")
+
+def celeb_un():
+        try:
+                user_id = api.user_id_from_username("iamduzz0909")
+                followings = api.user_following(user_id)
+                followings_ids = list(followings.keys())
+
+                for u_to_unfollow in followings_ids:
+                        try:
+                                print(f"\033[36m[>] Trying To Unfollow Celebrity")
+                                api.user_unfollow(u_to_unfollow)
+                        except instagrapi.exceptions.UserNotFound:
+                                print("\033[31m[>] User Not Found !")
+                        except instagrapi.exceptions.PleaseWaitFewMinutes:
+                                print("\n\033[31m[>] Please Try Again After Few Minute !")
+                                break
+        except KeyboardInterrupt:
+                print("\n\033[31m[>] Keyboard Interrupt : Script Ended !")
+                sys.exit()
+        except instagrapi.exceptions.UserNotFound:
+                print("\n\033[31m[>] User Not Found !")
+
+print("\033[92m[1]  Start")
+print("\033[92m[2]  Remove")
+print("\033[91m[3]  Exit\n")
+
+
+opt = int(input("\033[36mYour Option : "))
+print("\n\033[92m[+] Request Accepted\n")
+
+if opt == 1:
+	celeb()
+
+elif opt == 2:
+	celeb_un()
+
+elif opt == 3:
+	print("\n\033[36m[~] Thank You For Using Our Tool\n")
+	sys.exit()
+
+else:
+	print("Incorrect Option !!\n")
+	sys.exit()
+
+print("\n\033[36m[~] Thank You For Using Our Tool")
